b371ab997fe3afea45cc66c33f89f459a704a380
viededario <darioocceus@gmail.com>
2025-02-04 15:55:10 -0500

    added pieces on board and made them moveable

--- app.js
+++ app.js
@@ -3,6 +3,40 @@ const playerDisplay = document.querySelector("#player")
 const infoDisplay = document.querySelector("#info-display")
 const width = 8
 
-const firstPieces = [
-    
+const startPieces = [
+    rook, knight, bishop, queen, king, bishop, knight, rook,
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+    rook, knight, bishop, queen, king, bishop, knight, rook
 ]
+
+const createBoard = () => {
+    startPieces.forEach((startPiece, i) => {
+        const square = document.createElement("div")
+        square.classList.add("square")
+        square.innerHTML = startPiece
+        square.firstChild?.setAttribute("draggable", "true")
+        square.setAttribute("square-id", i)
+        const row = Math.floor((63 - i) / 8) + 1
+        if (row % 2 === 0) {
+                square.classList.add(i % 2 === 0 ? "beige" : "brown") 
+            } else {
+                square.classList.add(i % 2 === 0 ? "brown" : "beige")
+            }
+
+        if (i <= 15) { 
+            square.firstChild.firstChild.classList.add("black")
+        }
+        if (i>= 48) {
+            square.firstChild.firstChild.classList.add("white") 
+        } 
+        boardGame.append(square) 
+    })
+
+}
+
+createBoard();
