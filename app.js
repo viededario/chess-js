06034335faeba377b38048dd8239d4e3f9899570
viededario <darioocceus@gmail.com>
2025-02-03 19:31:58 -0500

    building chess, created files and added pieces

---
+++ app.js
@@ -0,0 +1,8 @@
+const boardGame = document.querySelector("#boardGame")
+const playerDisplay = document.querySelector("#player")
+const infoDisplay = document.querySelector("#info-display")
+const width = 8
+
+const firstPieces = [
+    
+]
